7aaf7fcfd0203048ddbc564d293386ca06fb7b3c
Dennis194 <dennis.dittmann194@gmail.com>
2026-01-26 23:28:16 +0100

    chair-optout: step 6 - csv export

--- byceps/blueprints/chair_optout/admin/views.py
+++ byceps/blueprints/chair_optout/admin/views.py
@@ -11,9 +11,10 @@ from flask import abort
 from byceps.services.chair_optout import chair_optout_service
 from byceps.services.party import party_service
 from byceps.services.party.models import Party
+from byceps.util.export import serialize_tuples_to_csv
 from byceps.util.framework.blueprint import create_blueprint
 from byceps.util.framework.templating import templated
-from byceps.util.views import permission_required
+from byceps.util.views import permission_required, textified
 
 
 blueprint = create_blueprint('chair_optout_admin', __name__)
@@ -34,6 +35,39 @@ def index(party_id):
         'party': party,
         'report_entries': report_entries,
     }
+
+
+@blueprint.get('/for_party/<party_id>/export.csv')
+@permission_required('chair_optout.export_report')
+@textified
+def export_as_csv(party_id):
+    """Export the chair opt-out report for a party as CSV."""
+    party = _get_party_or_404(party_id)
+
+    report_entries = chair_optout_service.get_report_entries_for_party(
+        party.id
+    )
+
+    header_row = (
+        'Name',
+        'Nickname',
+        'Ticketnummer',
+        'Sitzplatz-Label',
+    )
+
+    data_rows = [
+        (
+            entry.full_name or '',
+            entry.screen_name or '',
+            entry.ticket_code,
+            entry.seat_label or '',
+        )
+        for entry in report_entries
+    ]
+
+    rows = [header_row] + data_rows
+
+    return serialize_tuples_to_csv(rows)
 
 
 def _get_party_or_404(party_id) -> Party:
