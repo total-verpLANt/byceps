5b598f56ebae97da889539869f451fb4af06b96e
Cataklysim <cataklysim@web.de>
2026-02-01 01:16:04 +0100

    Remove todo because kevin doesnt like to read future planning in a commit

--- byceps/services/lan_tournament/tournament_service.py
+++ byceps/services/lan_tournament/tournament_service.py
@@ -43,23 +43,3 @@ def leave_tournament_team(tournament_id: TournamentId, participant_id: Tournamen
 
 def leave_tournament(tournament_id: TournamentId, participant_id: TournamentParticipantId) -> None:
     raise NotImplementedError
-
-# TODO: Do the converter like in the rest of byceps
-# def _db_entity_to_tournament(db_tournament: DbTournament) -> Tournament:
-#     """Convert database entity to domain model."""
-#     return Tournament(
-#         id=db_tournament.id,
-#         party_id=db_tournament.party_id,
-#         name=db_tournament.name,
-#         description=db_tournament.description,
-#         image_url=db_tournament.image_url,
-#         ruleset=db_tournament.ruleset,
-#         min_players=db_tournament.min_players,
-#         max_players=db_tournament.max_players,
-#         min_teams=db_tournament.min_teams,
-#         max_teams=db_tournament.max_teams,
-#         min_players_in_team=db_tournament.min_players_in_team,
-#         max_players_in_team=db_tournament.max_players_in_team,
-#         tournament_status=db_tournament.tournament_status,
-#         tournament_mode=db_tournament.tournament_mode,
-#     )
