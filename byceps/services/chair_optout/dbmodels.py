1458dc1e98cce07e06abaf618eedab1add85936b
MasterDD194 <dennis.dittmann194@gmail.com>
2026-02-05 23:30:44 +0100

    dbmodels.py aktualisieren

--- byceps/services/chair_optout/dbmodels.py
+++ byceps/services/chair_optout/dbmodels.py
@@ -13,7 +13,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from byceps.database import db
 from byceps.services.party.models import PartyID
 from byceps.services.ticketing.models.ticket import TicketID
-from byceps.services.user.models.user import UserID
+from byceps.services.user.models import UserID
 from byceps.util.uuid import generate_uuid7
 
 from .models import ChairOptoutID
